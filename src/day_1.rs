538a9b0ba538dbefe74a7276a8c2378e7821fd3c
Rafe <rsymonds@umich.edu>
2024-12-01 23:29:39 -0500

    Rust is cool i guess

---
+++ src/day_1.rs
@@ -0,0 +1,37 @@
+use std::collections::HashMap;
+use std::io::BufRead;
+
+pub fn part_1() -> usize {
+    let reader = std::io::BufReader::new(std::fs::File::open("input/day_1.txt").unwrap());
+
+    let mut lhs: Vec<usize> = Vec::new();
+    let mut rhs: Vec<usize> = Vec::new();
+
+    reader.lines().map(|l| l.unwrap()).for_each(|line| {
+        let mut sw = line.split_whitespace();
+        lhs.push(sw.next().unwrap().parse::<usize>().unwrap());
+        rhs.push(sw.next().unwrap().parse::<usize>().unwrap());
+    });
+    lhs.sort();
+    rhs.sort();
+
+    lhs.iter()
+        .zip(rhs.iter())
+        .map(|(l, r)| l.abs_diff(*r))
+        .sum()
+}
+
+pub fn part_2() -> usize {
+    let reader = std::io::BufReader::new(std::fs::File::open("input/day_1.txt").unwrap());
+    let mut lhs: Vec<usize> = Vec::new();
+    let mut rhs: HashMap<usize, usize> = HashMap::new();
+
+    reader.lines().map(|l| l.unwrap()).for_each(|line| {
+        let mut sw = line.split_whitespace();
+        lhs.push(sw.next().unwrap().parse::<usize>().unwrap());
+        *rhs.entry(sw.next().unwrap().parse::<usize>().unwrap())
+            .or_insert(0) += 1;
+    });
+
+    lhs.iter().map(|l| *l * *rhs.entry(*l).or_default()).sum()
+}
